3c9e994e6a106dd3b27f0e0b1c7a8ffeb0d7e97c
Rayan <rayanraihani9@gmail.com>
2022-12-01 15:48:59 +0100

    class poo livre

---
+++ LivrePoo/Livre.php
@@ -0,0 +1,49 @@
+<h1>Livre de Stephen King</h1>
+<?php
+
+
+class Livre{
+    private string $_titre;
+    private int $_annee;
+    private int $_page;
+    private int $_prix;
+
+    function __construct(string $titre, int $annee, int $page, int $prix,){
+        $this->_titre = $titre;
+        $this->_annee = $annee;
+        $this->_page = $page;
+        $this->_prix = $prix;
+    }
+    public function getTitre():string{
+        return $this->_titre;
+    }
+    public function setTitre(string $titre){
+        $this->_titre = $titre ;
+        return $this;   
+    }
+    public function getAnnee():int{
+        return $this->_annee;
+    }
+    public function setAnnee(int $annee){
+        $this->_annee = $annee ;
+        return $this;
+    }
+    public function getPage():int{
+        return $this->_page;
+    }
+    public function setPage(int $modele){
+        $this->_page = $page ;
+        return $this;
+    }
+    public function getPrix():int{
+        return $this->_prix;
+    }
+    public function setPrix(int $prix){
+        $this->_prix = $prix ;
+        return $this;
+    }
+
+}
+
+
+?>
