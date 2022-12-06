91da4c6c59df92fbec49878aab8851b10842518d
rayan <rayanraihani9@gmail.com>
2022-12-06 14:36:41 +0100

    rajout de array ainsi que la de auteur j ai mis nom prenom utilisitation de foreach a la fin et du echo pour afficher

--- LivrePoo/Auteur.php
+++ LivrePoo/Auteur.php
@@ -1,9 +1,42 @@
 <?php
 class Auteur{
-    private string $_auteur;
-    function __construct(string $auteur){
-        $this->_auteur = $auteur
+    private string $_nom;
+    private string $_prenom;
+    private array $_livres;
+
+    function __construct(string $nom, string $prenom){
+        $this->_nom = $nom;
+        $this->_prenom = $prenom;
+        $this->_livres = [];
     }
 
+    public function addLivre($livre){
+        $this->_livres[]=$livre;
+    }
+    public function getNom():string{
+        return $this->_nom;
+    }
+    public function setNom(string $nom){
+        $this->_nom = $nom ;
+        return $this;   
+    }
+    public function getPrenom():string{
+        return $this->_prenom;
+    }
+    public function setPrenom(string $prenom){
+        $this->_prenom = $prenom ;
+        return $this;   
+    }
+    public function __toString()
+    {
+        return $this->getNom() .$this->getPrenom(). ". <br>";
+    }
+    public function afficherBibliographie(){
+        echo "Livres de ". $this . "<br>";
+        foreach($this->_livres as $livre){
+            echo $livre ."<br>";
+     }
+    }
+    
 }
 ?>
